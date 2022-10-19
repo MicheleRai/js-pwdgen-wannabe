0315081476369a886e20eeba7a34ae7960006197
MicheleRai <rainonemichele1999@gmail.com>
2022-10-19 14:03:19 +0200

    other estetical improve added

--- js/code.js
+++ js/code.js
@@ -5,13 +5,16 @@
 
 let nome = prompt('Inserire nome utente');
 document.getElementById('nome').innerHTML = nome;
+console.log('nome:' + nome);
 
 let cognome = prompt('Inserire cognome utente');
 document.getElementById('cognome').innerHTML = cognome;
+console.log('cognome:' + cognome);
 
 let colore = prompt('Inserire colore preferito utente');
 document.getElementById('colore').innerHTML = colore;
+console.log('colore:' + colore);
 
-let pass = nome + cognome + colore + 22
-document.getElementById('pass').innerHTML = pass
-console.log(pass)
+let pass = nome + cognome + colore + 22;
+document.getElementById('pass').innerHTML = pass;
+console.log('password:' + pass);
